9f49d42dc60d1374213127c4223a9c5e66673dcf
Alexandr Metelkov <metel-sanya@mail.ru>
2023-06-10 16:35:15 +0300

    ref js

--- js/index.js
+++ js/index.js
@@ -1,17 +1,39 @@
 // объявление переменной
-let postTitle = '';
+let post = '';
 
 
-const titleInputNode = document.querySelector('.js-title-input'); // doc.query - вытаскивает класс из хтмл для рабоыт в js
+const titleInputNode = document.querySelector('.js-title-input'); // doc.query - вытаскивает класс из хтмл для работы в js
 const newPostBtnNode = document.querySelector('.js-new-post-btn');
 const postsNode = document.querySelector('.js-posts');
 
 newPostBtnNode.addEventListener('click', function() { // addEventListener - обработчик события . обратботка события (клик)
 
     // получить данные из поля ввода
+    const postFromUser = getPostFromUser();
+
+    setPost(postFromUser);
+
+    renderPost();
 
     // сохранить пост
-    postTitle= titleInputNode.value;
+
+    // отобразить пост
+
+    postTitle = titleInputNode.value; // вывод равен введенному тексту в input
 
     postsNode.innerText = postTitle;
 });
+
+function getPostFromUser() {
+    const post = titleInputNode.value;
+
+    return post;
+};
+
+function setPost(newPost) {
+    post = newPost;
+};
+
+function renderPost() {
+    postsNode.innerText = post;
+}
