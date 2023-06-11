b44332cac9e28a254d49c0b7d600a96176cd8161
Alexandr Metelkov <metel-sanya@mail.ru>
2023-06-11 22:15:14 +0300

    change js function

--- js/index.js
+++ js/index.js
@@ -1,39 +1,92 @@
 // объявление переменной
 const posts = [];
 
+const TITLE_VALIDATION_LIMIT = 10;
+const TEXT_VALIDATION_LIMIT = 20;
 
-const postTitleInputNode = document.querySelector(".js-post-title-input"); // doc.query - вытаскивает класс из хтмл для работы в js
+const postTitleInputNode = document.querySelector(".js-post-title-input");
 const postTextInputNode = document.querySelector(".js-post-text-input");
 const newPostBtnNode = document.querySelector(".js-new-post-btn");
 const postsNode = document.querySelector(".js-posts");
-
- let now = new Date();
- console.log(now)
-
+const validationMessage = document.getElementById("validationMessage");
 
 newPostBtnNode.addEventListener("click", function () {
-  // addEventListener - обработчик события . обратботка события (клик)
-
-  // получить данные из поля ввода
   const postFromUser = getPostFromUser();
 
   addPost(postFromUser);
 
   renderPosts();
-
-  // сохранить пост
-
-  // отобразить пост
-
-  // postTitle = postTitleInputNode.value; // вывод равен введенному тексту в input
-
-  // postsNode.innerText = postTitle;
 });
+
+// 1. postTitleInputNode.addEventListener("input", function (event) {
+//   //хотя тут можно было обойтись и без event.target, а напрямую проверить postTitleInputNode.value
+//   const currentValue = event.target.value;
+//   if (currentValue.length > TITLE_VALIDATION_LIMIT) {
+//     //вынесли в константы значение TITLE_VALIDATION_LIMIT
+//     validationMessage.innerText = `Длина заголовка не должна превышать ${TITLE_VALIDATION_LIMIT} символов`;
+//     validationMessage.classList.remove('validationMessage_hidden');
+//   } else {
+//     validationMessage.classList.add('validationMessage_hidden');
+//   }
+// });
+
+
+// 2. Что такое хендлер?
+// Handler – это класс, который используется для работы с очередью сообщений, связанной с потоком.
+// postTitleInputNode.addEventListener("input", function () {
+//   validation();
+// });
+
+
+// 3.
+postTitleInputNode.addEventListener("input", validation);
+
+// 1.  мы просто скопировали кусок кода выше и немного подправили его под валидацию длины текста
+// postTextInputNode.addEventListener("input", function (event) {
+//   const currentValue = event.target.value;
+//   if (currentValue.length > TEXT_VALIDATION_LIMIT) {
+//     //TODO: остается проблема - если у нас уже показано сообщение о валидации по длине заголовка
+//     //то если начать вводить текст поста, то предупреждение пропадет
+//     validationMessage.innerText = `Длина текста не должна превышать ${TEXT_VALIDATION_LIMIT} символов`;
+//     validationMessage.classList.remove('validationMessage_hidden');
+//   } else {
+//     validationMessage.classList.add('validationMessage_hidden');
+//   }
+// });
+
+// 2. Что такое хендлер?
+// Handler – это класс, который используется для работы с очередью сообщений, связанной с потоком.
+// postTextInputNode.addEventListener("input", function () {
+//   validation();
+// });
+
+
+// 3.
+postTextInputNode.addEventListener("input", validation);
+
+
+function validation() {
+  const titleLen = postTitleInputNode.value.length;
+  const textLen = postTextInputNode.value.length;
+
+  if (titleLen > TITLE_VALIDATION_LIMIT) {
+    validationMessage.innerText = `Длина заголовка не должна превышать ${TITLE_VALIDATION_LIMIT} символов`;
+    validationMessage.classList.remove("validationMessage_hidden");
+    return;
+  }
+
+  if (textLen > TEXT_VALIDATION_LIMIT) {
+    validationMessage.innerText = `Длина текста не должна превышать ${TEXT_VALIDATION_LIMIT} символов`;
+    validationMessage.classList.remove("validationMessage_hidden");
+    return;
+  }
+
+  validationMessage.classList.add("validationMessage_hidden");
+}
 
 function getPostFromUser() {
   const title = postTitleInputNode.value;
   const text = postTextInputNode.value;
-
 
   return {
     title: title,
@@ -50,7 +103,6 @@ function addPost({ title, text }) {
 
 function getPost() {
   return posts;
-
 }
 
 function renderPosts() {
