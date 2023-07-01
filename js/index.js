2a309f7f87bcd98dddf4066dd3c4f63fa4c7166e
Alexandr Metelkov <metel-sanya@mail.ru>
2023-07-01 14:33:29 +0300

    add resetPostBtn

--- js/index.js
+++ js/index.js
@@ -5,109 +5,112 @@ const date = new Date();
 const symbolTitleLimit = 50;
 const symbolTextLimit = 100;
 
- const postTitleInputNode = document.querySelector('.js-post__title__input');
- const postTextInputNode = document.querySelector('.js-post__text__input');
- const newPostBtnNode = document.querySelector('.js-new_post_btn');
- const resetPostBtnNode = document.querySelector('.js-reset_post_btn');
- const postsNode = document.querySelector('.js-posts');
+const postTitleInputNode = document.querySelector(".js-post__title__input");
+const postTextInputNode = document.querySelector(".js-post__text__input");
+const newPostBtnNode = document.querySelector(".js-new__post__btn");
+const resetPostBtnNode = document.querySelector(".js-reset__post__btn");
+const resetBlogBtnNode = document.querySelector(".js-reset__blog__btn");
+const postsNode = document.querySelector(".js-posts");
 
- const postTitleSymbols = document.querySelector('.js-post__title__symbols');
- const postTextSymbols = document.querySelector('.js-post__text__symbols');
-
+const postTitleSymbols = document.querySelector(".js-post__title__symbols");
+const postTextSymbols = document.querySelector(".js-post__text__symbols");
 
 // по клику кнопки будет срабатывать (обработчик события)
- newPostBtnNode.addEventListener('click', function() {  
-
- // получить пост от пользователя
+newPostBtnNode.addEventListener("click", function () {
+  // получить пост от пользователя
   const postFromUser = getPostFromUser();
 
   //вызов (присваиваем значение, которое ввел пользователь "post")
   addPost(postFromUser);
 
-//отображение поста
+  //отображение поста
   renderPosts();
+});
 
- });
+resetPostBtnNode.addEventListener("click", function () {
+  resetPost();
+});
 
+function resetPost() {
+  postTitleInputNode.value = "";
+  postTextInputNode.value = "";
+}
 
- resetPostBtnNode.addEventListener('click', function() {
+resetBlogBtnNode.addEventListener("click", function () {
+  resetBlog()
+});
 
-resetPost()
+function resetBlog() {
+  let test = document.querySelectorAll('.posts'); 
 
- });
+  for( let i = 0; i < test.length; i++ ){
+    test[i].outerHTML = "";
+  }
 
- function resetPost() {
+}
 
-   postTitleInputNode.value = '';
-   postTextInputNode.value = '';
- }
- 
-
- // получить пост от пользователя
- function getPostFromUser() { 
-
+// получить пост от пользователя
+function getPostFromUser() {
   //Обрабатываем ввод заголовка и описания
-  const title = postTitleInputNode.value;  // получаем заголовок из поля ввода заголовок
+  const title = postTitleInputNode.value; // получаем заголовок из поля ввода заголовок
   const text = postTextInputNode.value; // получаем описание из поля ввода напиши текст
 
   // возвращаем объект из title и text
   return {
     title,
-    text
+    text,
   };
- }
- 
-//добавление в массив
- function addPost({ date, title, text }) { 
+}
 
-  if (!title || !text)  return;
+//добавление в массив
+function addPost({ title, text }) {
+  if (!title || !text) return;
 
   resetPost();
 
   posts.push({
     date: `${getDate()}`,
     title,
-    text
+    text,
   });
- }
+}
 
- // получить посты
- function getPosts() {
+// получить посты
+function getPosts() {
   return posts;
- }
+}
 
 //отображение одного поста
- function renderPosts() {
+function renderPosts() {
   const posts = getPosts();
 
-  // формирование ХТМЛ через шаблонные строки внутри js 
-  let postsHTML = '';
+  // формирование ХТМЛ через шаблонные строки внутри js
+  let postsHTML = "";
 
-  posts.forEach(post => {  // добавление отдельного поста
-    postsHTML +=  ` 
+  posts.forEach((post) => {
+    // добавление отдельного поста
+    postsHTML += ` 
       <div class ='post'>
         <p class='post__date'>${post.date}</p>
         <p class='post__title'>${post.title}</p>
         <p class='post__text'>${post.text}</p>
       </div>
-    `
+    `;
   });
 
   postsNode.innerHTML = postsHTML;
- }
-
-function getDate() {
-  
-  const option = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-  };
-  
- const date = new Date().toLocaleString("ru", option)
-
-return date;
 }
 
+function getDate() {
+  const option = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  };
+
+  const date = new Date().toLocaleString("ru", option);
+
+  return date;
+}
