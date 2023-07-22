7abe2917b62afcf63fa9b13e468060e802f61be1
Alexandr Metelkov <metel-sanya@mail.ru>
2023-07-22 23:37:35 +0300

    refact js

--- js/index.js
+++ js/index.js
@@ -1,49 +1,61 @@
 const posts = [];
 
-const symbolTitleLimit = 'Заголовок больше 50 символов';
-const symbolTextLimit = 'Пост больше 100 символов';
-
+const limitTitle = 10;
+const limitText = 20;
+// достаем все нужные нам элементы из htmL
 const postTitleInputNode = document.querySelector(".js-post__title__input");
 const postTextInputNode = document.querySelector(".js-post__text__input");
+const validationTitle = document.querySelector(".js-validationTitle");
+const validationText = document.querySelector(".js-validationText");
+const validationMessage = document.querySelector(".js-validationMessage");
 const newPostBtnNode = document.querySelector(".js-new__post__btn");
-const postsNode = document.querySelector(".js-posts");
 const resetPostBtnNode = document.querySelector(".js-reset__post__btn");
 const resetBlogBtnNode = document.querySelector(".js-reset__blog__btn");
+const postsNode = document.querySelector(".js-posts");
 
-// по клику кнопки будет срабатывать (обработчик события)
-newPostBtnNode.addEventListener("click", function () {
-  // получить пост от пользователя
-  const postFromUser = getPostFromUser();
+//обработчики события
+postTitleInputNode.addEventListener("input", validation);
+postTextInputNode.addEventListener("input", validation);
+newPostBtnNode.addEventListener("click", newPostBtnHandler);
+resetPostBtnNode.addEventListener("click", resetPost);
+resetBlogBtnNode.addEventListener("click", resetBlog);
 
-  //вызов (присваиваем значение, которое ввел пользователь "post")
-  addPost(postFromUser); //этот метод добавит новый пост в массив (но еще не отобразит на странице)
+function validation() {
+  // проверка (валидация) на символы в инпутах
+  const titleLength = postTitleInputNode.value.length;
+  const textLength = postTextInputNode.value.length;
 
-  //отображение поста
-  renderPosts();
+  if (titleLength > limitTitle) {
+    validationMessage.className = "validationMessage";
+    validationMessage.innerText = `Длина заголовка не должна превышать ${limitTitle} символов`;
+    newPostBtnNode.disabled = true;
+    return;
+  } else {
+    validationMessage.className = "validationMessage__hidden";
+    newPostBtnNode.disabled = false;
+  }
 
-});
-
-//по клику кнопки - вывод пустого поста
-resetPostBtnNode.addEventListener("click", function () {
-  resetPost();
-});
-
-function resetPost() {
-  postTitleInputNode.value = "";
-  postTextInputNode.value = "";
+  if (textLength > limitText) {
+    validationMessage.className = "validationMessage";
+    validationMessage.innerText = `Длина поста не должна превышать ${limitText} символов`;
+    newPostBtnNode.disabled = true;
+    return;
+  } else {
+    validationMessage.className = "validationMessage__hidden";
+    newPostBtnNode.disabled = false;
+  }
 }
 
-// по клику кнопки - удаляются посты
-resetBlogBtnNode.addEventListener("click", function () {
-  resetBlog();
-});
+function newPostBtnHandler() {
+  const postFromUser = getPostFromUser(); //присвоит значение, которое ввел пользователь
 
-function resetBlog() {
-  let resetPost = document.querySelectorAll('.js-posts'); 
+  addPost(postFromUser); //этот метод добавит новый пост в массив (но еще не отобразит на странице)
 
-  for( let i = 0; i < resetPost.length; i++ ){
-    resetPost[i].outerHTML = '';
-  }
+  validation();
+
+  renderPosts(); //отобразит пост на странице
+
+  resetPost(); //очистит инпуты
 }
 
 // получить пост от пользователя
@@ -51,17 +63,39 @@ function getPostFromUser() {
   //Обрабатываем ввод заголовка и описания
   const title = postTitleInputNode.value; // получаем заголовок из поля ввода заголовок
   const text = postTextInputNode.value; // получаем описание из поля ввода напиши текст
-
+  if (title === "") {
+    addPostBtnNode.disabled = true;
+    return;
+  }
+  if (text === "") {
+    addPostBtnNode.disabled = true;
+    return;
+  }
   // возвращаем объект из title и text
   return {
     title,
     text,
   };
 }
-//добавление в массив
-function addPost({ date, title, text }) {
 
-  if (!title || !text) return ;
+// по клику кнопки срабатывает очистка инпутов
+function resetPost() {
+  postTitleInputNode.value = "";
+  postTextInputNode.value = "";
+
+  validation();
+}
+
+//удаление постов
+function resetBlog() {
+  posts.length = [];
+
+  postsNode.innerHTML = "Тут пока пусто...";
+}
+
+//добавление поста в массив
+function addPost({ title, text }) {
+  if (!title || !text) return;
 
   resetPost();
 
@@ -70,10 +104,24 @@ function addPost({ date, title, text }) {
     title,
     text,
   });
-
 }
 
-// получить посты
+//получение даты
+function getDate() {
+  const option = {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  };
+
+  const date = new Date().toLocaleString("ru", option);
+
+  return date;
+}
+
+// получение постов
 function getPosts() {
   return posts;
 }
@@ -81,7 +129,7 @@ function getPosts() {
 //отображение одного поста
 function renderPosts() {
   const posts = getPosts();
-  // формирование ХТМЛ через шаблонные строки внутри js
+  // формирование HTML через шаблонные строки внутри JS
   let postsHTML = "";
 
   posts.forEach((post) => {
@@ -96,19 +144,4 @@ function renderPosts() {
   });
 
   postsNode.innerHTML = postsHTML;
-
-}
-
-function getDate() {
-  const option = {
-    year: "numeric",
-    month: "numeric",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-  };
-
-  const date = new Date().toLocaleString("ru", option);
-
-  return date;
 }
